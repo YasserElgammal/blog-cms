12b628b94116122d320d29c5ada3fb7e5a683805
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-02-08 23:54:56 +0200

    Upgrade to Laravel 10

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('cache:prune-stale-tags')->hourly();
         // $schedule->command('inspire')->hourly();
     }
 
