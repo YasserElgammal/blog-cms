5e3b00fc98407be16405d6126b674696a17d3eb6
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-05-14 23:53:37 +0200

    update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,7 +23,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'role_id' => rand(1,3)
+            'role_id' => rand(1,3),
+            'news_letter' => rand(0,1)
         ];
     }
 
