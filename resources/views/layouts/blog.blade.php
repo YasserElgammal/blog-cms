7843b1be460f1deea72ad970e6085c79f229ba76
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-05-22 15:19:10 +0300

    Update Blog header

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -4,7 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@isset($title) {{ ucfirst($title) }} -@endisset {{ config('app.name') }}</title>
+    <title>
+        @isset($title)
+            {{ ucfirst($title) }} -
+        @endisset {{ config('app.name') }}
+    </title>
     <!-- Tailwind -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
     <style>
@@ -32,8 +36,19 @@
                 <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
                     @foreach ($pages_nav as $page)
                         <li><a class="hover:text-gray-200 hover:underline px-4"
-                                href="{{ route('page.show',$page->slug) }}">{{ $page->name }}</a></li>
+                                href="{{ route('page.show', $page->slug) }}">{{ $page->name }}</a></li>
                     @endforeach
+
+                    @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button class="py-2 px-4 bg-red-500 hover:bg-red-700">LogOut</button>
+                        </form>
+                    @else
+                    <li><a class="py-2 px-4 mr-2 bg-gray-500 hover:bg-gray-700" href="{{ route('register') }}">Register</a>
+                    <li><a class="py-2 px-4 bg-green-500 hover:bg-green-700" href="{{ route('login') }}">Login</a>
+                    @endauth
+
                 </ul>
             </nav>
 
@@ -97,68 +112,68 @@
 
         <!-- Sidebar Section -->
         @if (!request()->routeIs('page.show'))
-        <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
+            <aside class="w-full md:w-1/3 flex flex-col items-center px-3">
 
-            <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                <p class="text-xl font-semibold pb-5">About Us</p>
-                <p class="pb-2">{{ $setting->about }}</p>
-                {{-- <a href="#"
+                <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+                    <p class="text-xl font-semibold pb-5">About Us</p>
+                    <p class="pb-2">{{ $setting->about }}</p>
+                    {{-- <a href="#"
                     class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
                     Get to know us
                 </a> --}}
-            </div>
-
-            <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                <p class="text-xl font-semibold pb-5">Tags</p>
-                <div class="flex flex-wrap">
-
-                    @foreach ($tags as $tag)
-
-                    <a href="{{ route('tag.show', $tag->name) }}"
-                        class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-full text-blue-700 bg-blue-100 border border-blue-300 ">
-                        <div class="p-1.5 text-xs font-normal leading-none max-w-full flex-initial">
-                            {{ $tag->name }}</div>
-                    </a>
-
-                    @endforeach
-
                 </div>
-            </div>
 
-            <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-                <p class="text-xl font-semibold pb-5">Top 5 Writers</p>
-                {{--  --}}
-                <div class="content flex justify-between py-2 w-full">
-                    <div class="px-2 justify-between">
-                        Name
+                <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+                    <p class="text-xl font-semibold pb-5">Tags</p>
+                    <div class="flex flex-wrap">
+
+                        @foreach ($tags as $tag)
+                            <a href="{{ route('tag.show', $tag->name) }}"
+                                class="flex justify-center items-center m-1 font-medium py-1 px-2 bg-white rounded-full text-blue-700 bg-blue-100 border border-blue-300 ">
+                                <div class="p-1.5 text-xs font-normal leading-none max-w-full flex-initial">
+                                    {{ $tag->name }}</div>
+                            </a>
+                        @endforeach
 
                     </div>
-                    <div class="justify-between">
-                        Posts Count
-                    </div>
                 </div>
-                @forelse ($top_users as $top)
-                    <div class="my-1.5 py-3	px-4 flex justify-center rounded-lg shadow-lg bg-white w-full ">
-                        @if ($top->avatar == null)
-                        <img src="{{ asset('import/assets/profile-pic-dummy.png') }}" class="w-10 h-10 rounded-full">
-                        @else
-                        <img class="w-10 h-10 rounded-full" src="{{ asset("storage/$top->avatar") }}" alt="">
-                        @endif
-                        <div class="content flex justify-between py-2 w-full">
-                            <div class="px-2 justify-between">
-                                {{ $top->name }}
-                            </div>
-                            <div class="justify-between">
-                                {{ $top->posts_count }}
-                            </div>
+
+                <div class="w-full bg-white shadow flex flex-col my-4 p-6">
+                    <p class="text-xl font-semibold pb-5">Top 5 Writers</p>
+                    {{--  --}}
+                    <div class="content flex justify-between py-2 w-full">
+                        <div class="px-2 justify-between">
+                            Name
+
+                        </div>
+                        <div class="justify-between">
+                            Posts Count
                         </div>
                     </div>
+                    @forelse ($top_users as $top)
+                        <div class="my-1.5 py-3	px-4 flex justify-center rounded-lg shadow-lg bg-white w-full ">
+                            @if ($top->avatar == null)
+                                <img src="{{ asset('import/assets/profile-pic-dummy.png') }}"
+                                    class="w-10 h-10 rounded-full">
+                            @else
+                                <img class="w-10 h-10 rounded-full" src="{{ asset("storage/$top->avatar") }}"
+                                    alt="">
+                            @endif
+                            <div class="content flex justify-between py-2 w-full">
+                                <div class="px-2 justify-between">
+                                    {{ $top->name }}
+                                </div>
+                                <div class="justify-between">
+                                    {{ $top->posts_count }}
+                                </div>
+                            </div>
+                        </div>
                     @empty
                         No Members !
-                @endforelse
-            </div>
+                    @endforelse
+                </div>
 
-        </aside>
+            </aside>
         @endif
 
 
