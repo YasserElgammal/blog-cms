957def1e91579424a89e5ca7c3a0d8e891739758
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-06-01 17:00:35 +0300

    use CommentRequest

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
 use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -9,9 +10,9 @@ use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    public function makeComment(Request $request, Post $post)
+    public function makeComment(CommentRequest $request, Post $post)
     {
-        $validated = $request->validate(['body' => ['required', 'min:3']]);
+        $validated = $request->validated();
 
         $post->comments()->create([
             'user_id' => Auth::id(),
