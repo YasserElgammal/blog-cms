0ee74f1e869914532bc9c9e0b568a360b691253f
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-06-06 11:23:50 +0300

    Posts Seed Updates

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -22,6 +21,12 @@ class PostFactory extends Factory
     {
         $title = fake()->sentence();
         $slug = str($title)->slug();
+        $images_posts_path = 'storage/app/public/images/posts';
+
+        if (!File::exists($images_posts_path)) {
+            File::makeDirectory($images_posts_path);
+        }
+        
         return [
             'user_id' => rand(1,10),
             'category_id' => rand(1,5),
@@ -29,7 +34,7 @@ class PostFactory extends Factory
             'content' => fake()->paragraph(6),
             'status' => fake()->boolean(),
             'slug' => $slug,
-            'image' => 'dummy.jpg',
+            'image' => 'images/posts/' . fake()->image($images_posts_path,1000,500, null, false),
             'content' => fake()->paragraph(),
         ];
     }
