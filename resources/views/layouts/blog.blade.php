a63088bcb38359a3929008e52ea1e3dbdeb4c7eb
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-06-06 13:09:29 +0300

    update footer

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -203,7 +203,7 @@
         <div class="w-full container mx-auto flex flex-col items-center">
             <div class="flex flex-col md:flex-row text-center md:text-left md:justify-between py-6">
                 @foreach ($pages_footer as $page)
-                    <a href="{{ $page->slug }}" class="uppercase px-3 hover:text-blue-700">{{ $page->name }}</a>
+                    <a href="{{ route('page.show', $page->slug) }}" class="uppercase px-3 hover:text-blue-700">{{ $page->name }}</a>
                 @endforeach
             </div>
             <div class="uppercase pb-6">&copy; {{ $setting->copy_rights }}</div>
